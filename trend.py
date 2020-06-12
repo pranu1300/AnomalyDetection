bc1af676d37c94f2df5aed021e4a595542fd0f6e
Nikhilareddy57 <nikhilareddy57@gmail.com>
2020-06-12 16:52:16 +0530

    added period function

--- trend.py
+++ trend.py
@@ -4,6 +4,15 @@ from pandas import Series
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import STL
 
+def period(s):
+    w = np.fft.fft(s)
+    wabs = np.absolute(w)
+    wabs = np.delete(wabs, 0)
+    index = np.argmax(wabs)
+    index = index + 1
+    period = len(s)/index
+    return int(round(period))
+
 df = pd.read_csv("DailyDelhiClimate.csv")
 temp = df['meantemp'].values
 temp = pd.Series(temp, index=pd.date_range('1-1-2013', periods=len(temp), freq='D'), name = 'TMP')
@@ -16,4 +25,3 @@ plt.title("Trend graph")
 plt.ylabel("Temperature")
 plt.xlabel("Days")
 plt.show()
-
